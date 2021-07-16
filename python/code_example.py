be8bc2454b3204dca259dad35a42d409a1bb8ca9
Blake Burch <blake@shipyardapp.com>
2021-07-16 09:17:22 -0500

    Create code_example.py

---
+++ python/code_example.py
@@ -0,0 +1,13 @@
+import os, uuid0
+import pandas as pd
+import numpy as np
+
+print('Hello, World!')
+print(f'My name is {os.environ.get("NAME")}')
+print(f'Here\'s a UUID: {(uuid0.generate())}')
+print(f'''This Vessel is named "{os.environ.get("SHIPYARD_VESSEL_NAME")}" \
+and has an ID of {os.environ.get("SHIPYARD_VESSEL_ID")}''')
+
+df = pd.DataFrame(np.random.randint(1,100,size=(100000,4)),columns=list('ABCD'))
+df.to_csv(os.environ.get('FILE_NAME','output.csv'))
+print(df)
