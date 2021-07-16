299f169a5d6704840d515f8c99f071871ecb2a98
Blake Burch <blake@shipyardapp.com>
2021-07-16 09:24:18 -0500

    Update code_example.py

--- python/code_example.py
+++ python/code_example.py
@@ -2,6 +2,8 @@ import os, uuid0
 import pandas as pd
 import numpy as np
 
+file_name = os.environ.get('FILE_NAME','output.csv')
+
 print('Hello, World!')
 print(f'My name is {os.environ.get("NAME")}')
 print(f'Here\'s a UUID: {(uuid0.generate())}')
@@ -9,5 +11,6 @@ print(f'''This Vessel is named "{os.environ.get("SHIPYARD_VESSEL_NAME")}" \
 and has an ID of {os.environ.get("SHIPYARD_VESSEL_ID")}''')
 
 df = pd.DataFrame(np.random.randint(1,100,size=(100000,4)),columns=list('ABCD'))
-df.to_csv(os.environ.get('FILE_NAME','output.csv'))
+df.to_csv(file_name)
+print(f'File successfully created at {file_name}'
 print(df)
